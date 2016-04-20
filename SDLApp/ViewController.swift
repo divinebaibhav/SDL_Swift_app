fa6e2e7e1465d9c639d868dd9e27c5c5aa43e8d9
divinebaibhav <divinebaibhav@gmail.com>
2016-04-20 13:14:09 +0530

    initial commit

---
+++ SDLApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDLApp
+//
+//  Created by essadmin on 4/20/16.
+//  Copyright © 2016 Baibhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
